ebdb770e2daa1bded8897a5bfad084c79888afd5
mkopsch <martin.kopsch@gmx.net>
2021-08-11 11:48:53 +0200

    inject persistence settings

---
+++ Application/Infrastructure/Persistence/PersistenceSettings.cs
@@ -0,0 +1,9 @@
+namespace MaterialWebAPI.Infrastructure.Persistence
+{
+    public class PersistenceSettings
+    {
+        public string DatabaseName { get; set; }
+
+        public string[] Urls { get; set; }
+    }
+}
